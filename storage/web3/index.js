1cf61c51dfd2ddbae454d0f5c01579a3b7941eec
Ryan Detzel <ryandetzel@gmail.com>
2022-02-18 17:59:42 -0500

    store for web3 is working

---
+++ storage/web3/index.js
@@ -0,0 +1,23 @@
+import "dotenv/config";
+import { Web3Storage, File, getFilesFromPath } from "web3.storage";
+
+const WEB3_STORAGE_KEY = process.env.WEB3_STORAGE_KEY;
+const client = new Web3Storage({ token: WEB3_STORAGE_KEY });
+
+export class Web3StorageHelper {
+  constructor() {}
+
+  async storeFiles(filePath) {
+    const files = await getFilesFromPath(filePath);
+    const cid = await client.put(files);
+    return cid;
+  }
+
+  async storeContent(name, content) {
+    console.log("content", content);
+    const buffer = Buffer.from(content);
+    const file = [new File([buffer], name)];
+    const cid = await client.put(file);
+    return cid;
+  }
+}
