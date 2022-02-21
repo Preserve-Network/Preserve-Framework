b7bde07da51aedfd4325f167677f4c83c48967b3
Ryan Detzel <ryandetzel@gmail.com>
2022-02-21 08:38:09 -0500

    moved examples

---
+++ examples/example.js
@@ -0,0 +1,20 @@
+// Store File
+const Preserve = require("../index.js");
+
+(async () => {
+  const preserve = new Preserve();
+
+  const hash = await preserve.preserveFiles({
+    name: "single file test",
+    description: "desc",
+    files: ["testdata/test1.txt", "testdata/test2.txt"],
+    attributes: {
+      hello: "world",
+    },
+  });
+
+  console.log(hash);
+  console.log(await preserve.getLastValue());
+})().catch((e) => {
+  console.log(e);
+});
